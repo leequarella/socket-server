beef9826aa937285d15cc74f13607b997627ebf2
leequarella <leequarella@leequarella-laptop.(none)>
2012-06-12 15:50:13 -0400

    Moved client logic into client class

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -3,6 +3,8 @@
 
   Client = (function() {
 
+    Client.name = 'Client';
+
     function Client(socket, channel, nickname) {
       this.socket = socket;
       this.channel = channel;
@@ -11,11 +13,64 @@
     }
 
     Client.prototype.setChannel = function(channel) {
-      return this.channel = channel;
+      var _this = this;
+      this.leaveCurrentChannel();
+      this.channel = channel;
+      this.socket.set('channel', channel, function() {
+        if (_this.nickname) {
+          return _this.socket.broadcast.to(channel).emit("channel message", {
+            userName: "Server",
+            mes: _this.nickname + " has joined the channel."
+          });
+        } else {
+          return _this.socket.emit('channel message', {
+            userName: "Server",
+            mes: "Changed to channel " + _this.channel
+          });
+        }
+      });
+      return this.socket.join(channel);
     };
 
     Client.prototype.setNickname = function(nickname) {
-      return this.nickname = nickname;
+      var _this = this;
+      this.nickname = nickname;
+      return this.socket.set('nickname', nickname, function() {
+        return _this.socket.broadcast.emit('channel message', {
+          userName: "Server",
+          mes: _this.nickname + ' has connected.'
+        });
+      });
+    };
+
+    Client.prototype.broadcast = function(message) {
+      return this.socket.broadcast.to(this.channel).emit("channel message", {
+        userName: this.nickname,
+        mes: message
+      });
+    };
+
+    Client.prototype.leaveChannel = function(channel) {
+      this.socket.broadcast.to(channel).emit("channel message", {
+        userName: "Server",
+        mes: this.nickname + " has left the channel."
+      });
+      return this.socket.leave(channel);
+    };
+
+    Client.prototype.leaveCurrentChannel = function(socket) {
+      this.leaveChannel(this.channel);
+      return this.channel = null;
+    };
+
+    Client.prototype.disconnect = function() {
+      console.log(this.nickname + " disconnected from channel " + this.channel + ".");
+      if (this.nickname) {
+        return this.socket.broadcast.to(this.channel).emit("channel message", {
+          userName: "Server",
+          mes: this.nickname + " has disconnected."
+        });
+      }
     };
 
     return Client;
--- src/coffee/client.coffee
+++ src/coffee/client.coffee
@@ -2,8 +2,35 @@ class Client
   constructor: (@socket, @channel, @nickname) ->
     @id = @socket.id
 
-  setChannel:(channel) -> @channel = channel
+  setChannel:(channel) ->
+    @leaveCurrentChannel()
+    @channel = channel
+    @socket.set 'channel', channel, ()=>
+      if @nickname
+        @socket.broadcast.to(channel).emit("channel message", { userName: "Server", mes: @nickname + " has joined the channel."})
+      else
+        @socket.emit('channel message', {userName: "Server", mes: "Changed to channel " + @channel})
+    @socket.join(channel)
 
-  setNickname: (nickname) -> @nickname = nickname
+  setNickname: (nickname) ->
+    @nickname = nickname
+    @socket.set 'nickname', nickname, ()=>
+      @socket.broadcast.emit 'channel message', {userName: "Server", mes: @nickname + ' has connected.'}
+
+  broadcast: (message) ->
+    @socket.broadcast.to(@channel).emit("channel message", { userName: @nickname, mes: message})
+
+  leaveChannel: (channel) ->
+    @socket.broadcast.to(channel).emit("channel message", { userName: "Server", mes: @nickname + " has left the channel."})
+    @socket.leave(channel)
+
+  leaveCurrentChannel: (socket) ->
+    @leaveChannel(@channel)
+    @channel = null
+
+  disconnect: ->
+    console.log @nickname + " disconnected from channel " + @channel + "."
+    if @nickname
+      @socket.broadcast.to(@channel).emit("channel message", { userName: "Server", mes: @nickname + " has disconnected."})
 
 exports.Client = Client
