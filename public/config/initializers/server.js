4038fa5ae6183c7c8ef2852a5e5ddc88a379b9ce
Dean <deanfreekinggalvin@gmail.com>
2014-06-03 11:51:33 -0400

    update

--- public/config/initializers/server.js
+++ public/config/initializers/server.js
@@ -6,15 +6,29 @@
       this.port = port;
       this.express = require('express');
       this.http = require('http');
+      this.https = require('https');
+      this.fs = require('fs');
+      this.options = {
+        key: this.fs.readFileSync('./private/key.pem'),
+        cert: this.fs.readFileSync('./private/cacert.pem')
+      };
       console.log("STARTING SERVER");
       this.startExpress();
-      this.startSocketIO();
+      this.startSecureSocketIO();
       this.startStaticService();
     }
 
     ServerInitializer.prototype.startExpress = function() {
       console.log(" ...preparing express server.");
       return global.app = this.express();
+    };
+
+    ServerInitializer.prototype.startSecureSocketIO = function() {
+      console.log(" ...preparing https for socket.io");
+      global.server = this.https.createServer(this.options, app);
+      global.io = require('socket.io').listen(server);
+      server.listen(this.port);
+      return console.log(" ...socket.io listening on port " + port);
     };
 
     ServerInitializer.prototype.startSocketIO = function() {
