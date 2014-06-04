70fb1716050e6053ee2bf459de2eacc11305ab13
Dean <deanfreekinggalvin@gmail.com>
2014-06-04 13:09:59 -0400

    ssl fix

--- public/config/initializers/server.js
+++ public/config/initializers/server.js
@@ -20,29 +20,23 @@
 
     ServerInitializer.prototype.startExpress = function() {
       console.log(" ...preparing express server.");
-      return global.app = this.express();
-    };
-
-    ServerInitializer.prototype.startSecureSocketIO = function() {
-      console.log(" ...preparing https for socket.io");
-      global.server = this.https.createServer(this.options, app);
-      global.io = require('socket.io').listen(server);
-      server.listen(this.port);
-      return console.log(" ...socket.io listening on port " + port);
+      global.app = this.express();
+      return app.set('port', process.env.PORT || this.port);
     };
 
     ServerInitializer.prototype.startSocketIO = function() {
       console.log(" ...preparing http for socket.io");
       global.server = this.https.createServer(this.options, app);
-      server.listen(this.port);
       this.httpserver = this.http.createServer(app);
-      this.httpserver.listen(3002);
       global.io = require('socket.io').listen(server);
       return console.log(" ...socket.io listening on port " + port);
     };
 
     ServerInitializer.prototype.startStaticService = function() {
       console.log(" ...preparing to serve static assets");
+      app.listen(process.env.PORT, function() {
+        return console.log("EXP Listening on port:" + process.env.PORT);
+      });
       app.use(this.express.bodyParser());
       app.use(this.express["static"](__dirname + '/static'));
       return console.log("..done.");
--- src/config/initializers/server.coffee
+++ src/config/initializers/server.coffee
@@ -12,34 +12,24 @@ class ServerInitializer
     console.log "STARTING SERVER"
     @startExpress()
     @startSocketIO()
-    #@startSecureSocketIOj)
     @startStaticService()
 
   startExpress: ->
     console.log " ...preparing express server."
     global.app = @express()
-    #app.set 'port', process.env.PORT || @port
-
-  startSecureSocketIO: ->
-    console.log " ...preparing https for socket.io"
-    global.server = @https.createServer(@options, app)
-    global.io = require('socket.io').listen(server)
-    server.listen @port
-    console.log " ...socket.io listening on port #{port}"
+    app.set 'port', process.env.PORT || @port
 
   startSocketIO: ->
     console.log " ...preparing http for socket.io"
     global.server = @https.createServer(@options, app)
-    server.listen @port
     @httpserver = @http.createServer(app)
-    @httpserver.listen 3002
     global.io = require('socket.io').listen(server)
     console.log " ...socket.io listening on port #{port}"
 
   startStaticService: ->
     console.log " ...preparing to serve static assets"
-    #app.listen process.env.PORT, () ->
-    #  console.log "EXP Listening on port:" + process.env.PORT
+    app.listen process.env.PORT, () ->
+      console.log "EXP Listening on port:" + process.env.PORT
     app.use @express.bodyParser()
     app.use @express.static(__dirname + '/static')
     console.log "..done."
